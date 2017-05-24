1253f9fdd8f5795dc1c3dda4b8e8eb4109816299
Finn-Olav <finnolav@gmail.com>
2017-05-24 07:59:08 +0200

    added tests

--- test.js
+++ test.js
@@ -1,11 +1,66 @@
-const expect = require('chai').expect;
-const delayed = require('./');
-
-describe('delayed', () => {
-  it('should wait 1000ms and call the function with the payload supplied:', done => {
-    delayed.create(1000, payload => {
-      expect(payload.message).to.equal('Yes!');
+var expect = require('chai').expect;
+var delayedCall = require('./');
+var globalthis = this;
+describe('delayed-call', function() {
+  it('should wait 1000ms and call the function with the payload supplied', function(done) {
+    var me = this;
+    delayedCall.create(1000, (count, message) => {
+      expect(count).to.equal(50);
+      expect(message).to.equal('hello');
       done();
-    }, {message: 'Yes!'});
+    }, 50, 'hello');
+  });
+
+  it('should work without a payload', function(done) {
+    delayedCall.create(500, () => {
+      done();
+    });
+  });
+
+  it('should handle this', function(done) {
+    var me = this;
+    delayedCall.create(500, () => {
+      expect(me).to.equal(this);
+      done();
+    });
+  });
+
+  it('should handle this and payload', function(done) {
+    var me = this;
+    delayedCall.create(500, () => {
+      expect(me).to.equal(this);
+      done();
+    }, "hello");
+  });
+
+  it('should handle bound function', function(done) {
+    var me = this;
+    delayedCall.create(500, (function() {
+      expect(me).to.equal(this);
+      done();
+    }).bind(this));
+  });
+
+  it('should handle bound function and payload', function(done) {
+    var me = this;
+    delayedCall.create(500, (function() {
+      expect(me).to.equal(this);
+      done();
+    }).bind(this), "hello");
+  });
+
+  it('should handle multiple delayed calls', function(done) {
+    var count = 0;
+    delayedCall.create(200, function() {
+      count++;
+    });
+    delayedCall.create(600, function() {
+      count++;
+    });
+    delayedCall.create(1000, function() {
+      count++;
+      expect(count).to.equal(3);
+      done();
+    });
   });
 });
