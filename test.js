44df034271cef01dd5f5bf90251133b3a617e66b
Finn-Olav <finnolav@gmail.com>
2017-05-26 14:09:14 +0200

    removed es6 features from tests

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
 var expect = require('chai').expect;
 var delayedCall = require('./');
-var globalthis = this;
+
 describe('delayed-call', function() {
   it('should wait 1000ms and call the function with the payload supplied', function(done) {
-    var me = this;
-    delayedCall.create(1000, (count, message) => {
+    delayedCall.create(1000, function(count, message) {
       expect(count).to.equal(50);
       expect(message).to.equal('hello');
       done();
@@ -12,25 +11,26 @@ describe('delayed-call', function() {
   });
 
   it('should work without a payload', function(done) {
-    delayedCall.create(500, () => {
+    delayedCall.create(500, function() {
       done();
     });
   });
 
   it('should handle this', function(done) {
     var me = this;
-    delayedCall.create(500, () => {
+    delayedCall.create(500, (function() {
       expect(me).to.equal(this);
       done();
-    });
+    }).bind(this));
   });
 
   it('should handle this and payload', function(done) {
     var me = this;
-    delayedCall.create(500, () => {
+    delayedCall.create(500, (function(arg1) {
       expect(me).to.equal(this);
+      expect(arg1).to.equal("hello");
       done();
-    }, "hello");
+    }).bind(this), "hello");
   });
 
   it('should handle bound function', function(done) {
