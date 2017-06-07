0141cfacf8c42d3ac5d0acba26dc6564b8b2b5d0
Finn-Olav <finnolav@gmail.com>
2017-06-07 08:51:14 +0200

    more coverage

--- test.js
+++ test.js
@@ -37,12 +37,42 @@ test.cb('should handle multiple delayed calls', t => {
   delayedCall.create(200, function () {
     count++;
   });
-  delayedCall.create(600, function () {
+  delayedCall.create(300, function () {
     count++;
   });
-  delayedCall.create(1000, function () {
+  delayedCall.create(4000, function () {
     count++;
     t.is(count, 3);
+    t.end();
+  });
+});
+
+test.cb('should clear all delayed calls', t => {
+  var count = 0;
+  delayedCall.create(200, function () {
+    count++;
+  });
+  delayedCall.create(200, function () {
+    count++;
+  });
+
+  delayedCall.clearAll();
+
+  delayedCall.create(400, function () {
+    t.is(count, 0);
+    t.end();
+  });
+});
+
+test.cb('should clear single delayed call', t => {
+  var count = 0;
+  var id = delayedCall.create(200, function () {
+    count++;
+  });
+  delayedCall.clearById(id);
+
+  delayedCall.create(300, function () {
+    t.is(count, 0);
     t.end();
   });
 });
